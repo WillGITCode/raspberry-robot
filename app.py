7ed02775ca5a12d2621b8a72974e902cbf9d4a99
William <wsilburn@ymail.com>
2021-05-29 19:47:16 -0400

    test sensor module import

--- app.py
+++ app.py
@@ -2,14 +2,21 @@
 # Setup and state overseen here
 import RPi.GPIO as GPIO
 import time
-from modules import ping 
-# Use board based pin numbering 
+from modules import ping
+# Use board based pin numbering
 GPIO.setmode(GPIO.BOARD)
 # Motor pins
-motors = [7,11,13,15]
+motors = [7, 11, 13, 15]
 # Servo 1 pin
 servo1 = 18
 # Ping 1 pin
 ping1 = 16
 
-print (GetDistance(ping1))
+
+try:
+    while True:
+        distance = ping.GetDistance(ping1)
+        print("Distance to object is ", distance, " cm")
+        time.sleep(1)
+finally:
+    GPIO.cleanup()
