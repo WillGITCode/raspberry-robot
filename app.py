ef0e13f95014877c16106c0bddc653dda0d952d4
William <wsilburn@ymail.com>
2021-11-07 21:14:00 -0500

    done debugging controller?

--- app.py
+++ app.py
@@ -15,82 +15,16 @@ servo1 = 18
 # Ping 1 pin
 ping1 = 16
 
-# ping test
-# try:
-#     while True:
-#         distance = ping.GetDistance(ping1)
-#         print("Distance to object is ", distance, " cm")
-#         time.sleep(1)
-# finally:
-#     GPIO.cleanup()
 
-# motor test
-# try:
-#     motor.driveForwards()
-#     time.sleep(3)
-#     motor.driveBackwards()
-#     time.sleep(3)
-#     motor.spinLeft()
-#     time.sleep(2)
-#     motor.spinRight()
-#     time.sleep(2)
-#     motor.driveStop()
-#     time.sleep(5)
-# finally:
-#     GPIO.cleanup()
+def avoidObstacles():
+    if ping.GetDistance(ping1) > 7:
+        motor.DriveForwards()
+    else:
+        motor.DriveBackwards()
+        time.sleep(1)
+        motor.SpinLeft()
+        time.sleep(.5)
 
-# ping/motor test
-# try:
-#     while True:
-#         if ping.GetDistance(ping1) > 7:
-#             motor.DriveForwards()
-#         else:
-#             motor.DriveBackwards()
-#             time.sleep(1)
-#             motor.SpinLeft()
-#             time.sleep(.5)
-# finally:
-#     GPIO.cleanup()
-
-# pygame test
-# try:
-#     while True:
-#         for event in pygame.event.get():
-#             if event.type == pygame.KEYDOWN:
-#                 if event.key == ord('q'):
-#                     pygame.quit()
-#                 elif event.key == pygame.K_UP:
-#                     print("up")
-#                     GPIO.output(7, True)
-#                     GPIO.output(11, False)
-#                     GPIO.output(13, True)
-#                     GPIO.output(15, False)
-#                 elif event.key == pygame.K_DOWN:
-#                     print("down")
-#                     GPIO.output(7, False)
-#                     GPIO.output(11, True)
-#                     GPIO.output(13, False)
-#                     GPIO.output(15, True)
-#                 elif event.key == pygame.K_RIGHT:
-#                     print("right")
-#                     GPIO.output(7, True)
-#                     GPIO.output(11, False)
-#                     GPIO.output(13, False)
-#                     GPIO.output(15, True)
-#                 elif event.key == pygame.K_LEFT:
-#                     print("left")
-#                     GPIO.output(7, False)
-#                     GPIO.output(11, True)
-#                     GPIO.output(13, True)
-#                     GPIO.output(15, False)
-#             elif event.type == pygame.KEYUP:
-#                 print("stop")
-#                 GPIO.output(7, False)
-#                 GPIO.output(13, False)
-#                 GPIO.output(11, False)
-#                 GPIO.output(15, False)
-# finally:
-#     GPIO.cleanup()
 
 # Controller test
 if __name__ == '__main__':
@@ -100,6 +34,9 @@ if __name__ == '__main__':
                 if gamePad.getProperty('Start') == 1 or gamePad.getProperty('Back'):
                     motor.DriveStop()
                     raise SystemExit(101)
+                if gamePad.getProperty('A') == 1:
+                    while gamePad.getProperty('A') != 1:
+                        avoidObstacles()
                 elif gamePad.getProperty('LeftJoystickY') >= 0.7:
                     # print("Backward")
                     while gamePad.getProperty('LeftJoystickY') >= 0.7:
@@ -108,6 +45,7 @@ if __name__ == '__main__':
                     # print("Forward")
                     while gamePad.getProperty('LeftJoystickY') <= -0.7:
                         motor.DriveForwards()
+                elif gamePad.getProperty('LeftJoystickX') >= 0.7:
                     # print("Left")
                     while gamePad.getProperty('LeftJoystickX') >= 0.7:
                         motor.DriveLeft()
--- modules/controller.py
+++ modules/controller.py
@@ -1,6 +1,5 @@
 import threading
 import math
-import time
 from inputs import get_gamepad
 
 
@@ -52,60 +51,56 @@ class XboxController(object):
 
     def _monitor_controller(self):
         while True:
-            try:
-                events = get_gamepad()
-                if events is not None:
-                    for event in events:
-                        # print(event.ev_type, event.code, event.state)
-                        if event.code == 'ABS_Y':
-                            self.LeftJoystickY = event.state / \
-                                XboxController.MAX_JOY_VAL  # normalize between -1 and 1
-                        elif event.code == 'ABS_X':
-                            self.LeftJoystickX = event.state / \
-                                XboxController.MAX_JOY_VAL  # normalize between -1 and 1
-                        elif event.code == 'ABS_RY':
-                            self.RightJoystickY = event.state / \
-                                XboxController.MAX_JOY_VAL  # normalize between -1 and 1
-                        elif event.code == 'ABS_RX':
-                            self.RightJoystickX = event.state / \
-                                XboxController.MAX_JOY_VAL  # normalize between -1 and 1
-                        elif event.code == 'ABS_Z':
-                            self.LeftTrigger = event.state / \
-                                XboxController.MAX_TRIG_VAL  # normalize between 0 and 1
-                        elif event.code == 'ABS_RZ':
-                            self.RightTrigger = event.state / \
-                                XboxController.MAX_TRIG_VAL  # normalize between 0 and 1
-                        elif event.code == 'BTN_TL':
-                            self.LeftBumper = event.state
-                        elif event.code == 'BTN_TR':
-                            self.RightBumper = event.state
-                        elif event.code == 'BTN_SOUTH':
-                            self.A = event.state
-                        elif event.code == 'BTN_NORTH':
-                            self.X = event.state
-                        elif event.code == 'BTN_WEST':
-                            self.Y = event.state
-                        elif event.code == 'BTN_EAST':
-                            self.B = event.state
-                        elif event.code == 'BTN_THUMBL':
-                            self.LeftThumb = event.state
-                        elif event.code == 'BTN_THUMBR':
-                            self.RightThumb = event.state
-                        elif event.code == 'BTN_SELECT':
-                            self.Back = event.state
-                        elif event.code == 'BTN_START':
-                            self.Start = event.state
-                        elif event.code == 'BTN_TRIGGER_HAPPY1':
-                            self.LeftDPad = event.state
-                        elif event.code == 'BTN_TRIGGER_HAPPY2':
-                            self.RightDPad = event.state
-                        elif event.code == 'BTN_TRIGGER_HAPPY3':
-                            self.UpDPad = event.state
-                        elif event.code == 'BTN_TRIGGER_HAPPY4':
-                            self.DownDPad = event.state
-            except:
-                # wait/try again
-                time.sleep(0.0001)
+            events = get_gamepad()
+            if events is not None:
+                for event in events:
+                    # print(event.ev_type, event.code, event.state)
+                    if event.code == 'ABS_Y':
+                        self.LeftJoystickY = event.state / \
+                            XboxController.MAX_JOY_VAL  # normalize between -1 and 1
+                    elif event.code == 'ABS_X':
+                        self.LeftJoystickX = event.state / \
+                            XboxController.MAX_JOY_VAL  # normalize between -1 and 1
+                    elif event.code == 'ABS_RY':
+                        self.RightJoystickY = event.state / \
+                            XboxController.MAX_JOY_VAL  # normalize between -1 and 1
+                    elif event.code == 'ABS_RX':
+                        self.RightJoystickX = event.state / \
+                            XboxController.MAX_JOY_VAL  # normalize between -1 and 1
+                    elif event.code == 'ABS_Z':
+                        self.LeftTrigger = event.state / \
+                            XboxController.MAX_TRIG_VAL  # normalize between 0 and 1
+                    elif event.code == 'ABS_RZ':
+                        self.RightTrigger = event.state / \
+                            XboxController.MAX_TRIG_VAL  # normalize between 0 and 1
+                    elif event.code == 'BTN_TL':
+                        self.LeftBumper = event.state
+                    elif event.code == 'BTN_TR':
+                        self.RightBumper = event.state
+                    elif event.code == 'BTN_SOUTH':
+                        self.A = event.state
+                    elif event.code == 'BTN_NORTH':
+                        self.X = event.state
+                    elif event.code == 'BTN_WEST':
+                        self.Y = event.state
+                    elif event.code == 'BTN_EAST':
+                        self.B = event.state
+                    elif event.code == 'BTN_THUMBL':
+                        self.LeftThumb = event.state
+                    elif event.code == 'BTN_THUMBR':
+                        self.RightThumb = event.state
+                    elif event.code == 'BTN_SELECT':
+                        self.Back = event.state
+                    elif event.code == 'BTN_START':
+                        self.Start = event.state
+                    elif event.code == 'BTN_TRIGGER_HAPPY1':
+                        self.LeftDPad = event.state
+                    elif event.code == 'BTN_TRIGGER_HAPPY2':
+                        self.RightDPad = event.state
+                    elif event.code == 'BTN_TRIGGER_HAPPY3':
+                        self.UpDPad = event.state
+                    elif event.code == 'BTN_TRIGGER_HAPPY4':
+                        self.DownDPad = event.state
 
 
 # if __name__ == '__main__':
