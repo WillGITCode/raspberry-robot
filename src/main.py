b81814be34837f12074774df40cf86b9111be0a6
William <wsilburn@ymail.com>
2021-11-09 20:49:19 -0500

    continue refactor, classes, modularization

--- src/main.py
+++ src/main.py
@@ -1,25 +1,25 @@
 import RPi.GPIO as GPIO
 from time import sleep
-from modules import ping
-from modules import motor
-from modules import controller
+from modules import ping_sensor
+from modules import motor_board
+from modules import control_surfaces
 # create controller
-gamePad = controller.XboxController()
-# Motor pins
-motor.init_motor_pins([7, 11, 13, 15])
+gamePad = control_surfaces.XboxController()
+# Motor board pins
+motor_board.init_motor_pins([7, 11, 13, 15])
+# Ping sensor
+ping1 = ping_sensor.PingSensor(16)
 # Servo 1 pin
 servo1 = 18
-# Ping 1 pin
-ping1 = 16
 
 
 def avoid_obstacles():
-    if ping.get_distance(ping1) > 7:
-        motor.drive_forwards()
+    if ping1.get_distance() > 7:
+        motor_board.drive_forwards()
     else:
-        motor.drive_backwards()
+        motor_board.drive_backwards()
         sleep(1)
-        motor.spin_left()
+        motor_board.spin_left()
         sleep(.5)
 
 
@@ -29,7 +29,7 @@ def main():
         while True:
             try:
                 if gamePad.get_property('Start') == 1 or gamePad.get_property('Back'):
-                    motor.drive_stop()
+                    motor_board.drive_stop()
                     raise SystemExit(101)
                 if gamePad.get_property('A') == 1:
                     sleep(.001)
@@ -38,29 +38,29 @@ def main():
                 elif gamePad.get_property('LeftJoystickY') >= 0.7:
                     # print("Backward")
                     while gamePad.get_property('LeftJoystickY') >= 0.7:
-                        motor.drive_backwards()
+                        motor_board.drive_backwards()
                 elif gamePad.get_property('LeftJoystickY') <= -0.7:
                     # print("Forward")
                     while gamePad.get_property('LeftJoystickY') <= -0.7:
-                        motor.drive_forwards()
+                        motor_board.drive_forwards()
                 elif gamePad.get_property('LeftJoystickX') >= 0.7:
                     # print("Left")
                     while gamePad.get_property('LeftJoystickX') >= 0.7:
-                        motor.drive_left()
+                        motor_board.drive_left()
                 elif gamePad.get_property('LeftJoystickX') <= -0.7:
                     # print("Right")
                     while gamePad.get_property('LeftJoystickX') <= -0.7:
-                        motor.drive_right()
+                        motor_board.drive_right()
                 elif gamePad.get_property('LeftTrigger') >= 0.7:
                     # print("spin_right")
                     while gamePad.get_property('LeftTrigger') >= 0.7:
-                        motor.spin_right()
+                        motor_board.spin_right()
                 elif gamePad.get_property('RightTrigger') >= 0.7:
                     # print("spin_left")
                     while gamePad.get_property('RightTrigger') >= 0.7:
-                        motor.spin_left()
+                        motor_board.spin_left()
                 else:
-                    motor.drive_stop()
+                    motor_board.drive_stop()
             finally:
                 sleep(0.0001)
     finally:
