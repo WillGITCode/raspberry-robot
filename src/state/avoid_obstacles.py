c55c69a80fb481f06dfc4743ebcf4ad3b6c9e7de
William <25758160+WillGITCode@users.noreply.github.com>
2023-09-11 21:54:55 -0400

    tweak avoid obs

--- src/state/avoid_obstacles.py
+++ src/state/avoid_obstacles.py
@@ -1,16 +1,34 @@
-from time import sleep
+from threading import Timer
+from time import time
 
 
 class AvoidObstaclesState:
     def __init__(self, motor_controller, ping_sensor):
         self.ping_sensor = ping_sensor
         self.motor_controller = motor_controller
-        
+        self.timer = None
+        self.last_time = time()
 
     def run(self):
         if self.ping_sensor.get_distance() > 7:
             self.motor_controller.drive_forwards()
+            self.cancel_timer()
         else:
             self.motor_controller.drive_backwards()
-            sleep(1)
+            self.start_timer()
+
+    def start_timer(self):
+        if self.timer is None:
+            self.timer = Timer(1.0, self.on_timer)
+            self.timer.start()
+            self.last_time = time()
+
+    def cancel_timer(self):
+        if self.timer is not None:
+            self.timer.cancel()
+            self.timer = None
+
+    def on_timer(self):
+        if time() - self.last_time >= 1.0:
             self.motor_controller.spin_left()
+            self.timer = None
