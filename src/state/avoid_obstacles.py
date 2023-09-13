444714fc618c4a3754ddb62caf5d1c4bb7603295
William <25758160+WillGITCode@users.noreply.github.com>
2023-09-12 21:07:17 -0400

    servo test

--- src/state/avoid_obstacles.py
+++ src/state/avoid_obstacles.py
@@ -1,6 +1,8 @@
 
 
 from time import sleep
+MINIMUM_DISTANCE = 7
+directions = ["left", "right", "turn_around"]
 
 
 class AvoidObstaclesState:
@@ -9,9 +11,31 @@ class AvoidObstaclesState:
         self.motor_controller = motor_controller
 
     def run(self):
-        if self.ping_sensor.get_distance() > 7:
+        if self.ping_sensor.get_distance() > MINIMUM_DISTANCE:
             self.motor_controller.drive_forwards(0.7)
         else:
             self.motor_controller.drive_stop()
             self.motor_controller.spin_left(1)
             sleep(0.5)
+
+    def get_optimal_direction(self):
+        left_distance
+        right_distance
+        self.ping_servo.look_left()
+        # delay ?
+        left_distance = self.ping_sensor.get_distance()
+        self.ping_servo.look_right()
+        # delay ?
+        right_distance = self.ping_sensor.get_distance()
+        # turn left if no optimal direction
+        if left_distance > MINIMUM_DISTANCE and right_distance > MINIMUM_DISTANCE:
+            return directions[0]
+        # turn around if left and right are too close
+        elif left_distance < MINIMUM_DISTANCE and right_distance < MINIMUM_DISTANCE:
+            return directions[2]
+        # turn left if right is too close
+        elif left_distance > right_distance:
+            return directions[0]
+        # turn right if left is too close
+        elif left_distance < right_distance:
+            return directions[1]
--- src/test.py
+++ src/test.py
@@ -13,8 +13,34 @@
 # print(s1 is s2)  # Output: True
 
 
-p = PingSensor(16)
+# p = PingSensor(16)
 
-while True:
-    print(p.get_distance())
+# while True:
+#     print(p.get_distance())
+#     time.sleep(0.5)
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(35, GPIO.OUT)
+servo = GPIO.PWM(35, 50)
+
+servo.start(0)
+print("Starting servo")
+time.sleep(2)
+
+print("Moving servo")
+duty = 2
+while duty <= 12:
+    servo.ChangeDutyCycle(duty)
     time.sleep(0.5)
+    duty += 1
+
+time.sleep(2)
+
+print("moving servo back")
+
+servo.ChangeDutyCycle(7)
+time.sleep(2)
