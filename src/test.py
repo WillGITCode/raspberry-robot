9435eaee72d88aaa71581bb4ecd23d511a69a377
William <25758160+WillGITCode@users.noreply.github.com>
2023-09-12 20:22:03 -0400

    test ping

--- src/test.py
+++ src/test.py
@@ -1,13 +1,20 @@
-class Singleton:
-    __instance = None
+# class Singleton:
+#     __instance = None
 
-    def __new__(cls):
-        if cls.__instance is None:
-            cls.__instance = super().__new__(cls)
-        return cls.__instance
+#     def __new__(cls):
+#         if cls.__instance is None:
+#             cls.__instance = super().__new__(cls)
+#         return cls.__instance
 
 
-s1 = Singleton()
-s2 = Singleton()
+# s1 = Singleton()
+# s2 = Singleton()
 
-print(s1 is s2)  # Output: True
+# print(s1 is s2)  # Output: True
+
+
+p = PingSensor(16)
+
+while True:
+    print(p.get_distance())
+    time.sleep(0.5)
