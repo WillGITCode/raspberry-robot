5f89236693ffe6d4f6570190fb2b2ea965bf7d25
William <25758160+WillGITCode@users.noreply.github.com>
2023-09-13 20:58:04 -0400

    more threading tests 2

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 from control.xbox_controller import XboxControllerSingleton
 from state.robot_state_machine import RobotStateMachine
 import RPi.GPIO as GPIO
+import threading
+
+
+def run_state_machine(state_machine):
+    while True:
+        state_machine.run()
 
 
 def main():
@@ -12,21 +18,33 @@ def main():
         # Set the initial state of the state machine
         state_machine.set_state("idle")
 
+        # Start the state machine thread
+        state_machine_thread = threading.Thread(
+            target=run_state_machine, args=(state_machine,))
+        state_machine_thread.start()
+
         while True:
             if controller.get_property('Back'):
+                # Stop the state machine thread
+                state_machine.stop()
+                state_machine_thread.join()
                 raise SystemExit(101)
 
             if controller.get_property('Start'):
+                # Set the next state of the state machine
                 state_machine.set_next_state("idle")
 
             if controller.get_property('A') == 1:
+                # Set the next state of the state machine
                 state_machine.set_next_state("avoid_obstacles")
 
             if controller.get_property('Y') == 1:
+                # Set the next state of the state machine
                 state_machine.set_next_state("remote_control_navigation")
 
-            state_machine.run()
-        
+            # Sleep for a short time to avoid high CPU usage
+            sleep(0.01)
+
     finally:
         print("Exiting main")
         GPIO.cleanup()
